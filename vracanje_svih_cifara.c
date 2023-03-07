a5d1847927437111b063a3c865e076e8884899a4
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-07 20:30:28 +0100

    .

--- vracanje_svih_cifara.c
+++ vracanje_svih_cifara.c
@@ -1,10 +1,10 @@
+// Uzima broj i okrece ga
 #include <stdio.h>
 int main(){
     unsigned long long a;
     scanf("%llu", &a);
     long t=a;
     int j;
-    //akgirutan ta dobijnje svih cifara broja
     while(t!=0){
         j=t%10;
         t/=10;
