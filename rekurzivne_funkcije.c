62c57ed430f771e923d8f842f31137ba7eec0761
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-07 20:51:16 +0100

    .

--- rekurzivne_funkcije.c
+++ rekurzivne_funkcije.c
@@ -4,6 +4,7 @@ int fakt(int a){
     if(a==0)
         return 1;
     return a * fakt(a-1);
+    
 }
 int main(){
     printf("%d", fakt(5));
--- test.c
+++ test.c
@@ -1,24 +1,3 @@
-/*
-#include <stdio.h>
-
-int main(){
-
-  int x,y;
-  printf("Uneti promenljivu x u dekadnom sistemu:",x);
-  scanf("%d", &x);
-  printf("Uneti realne promenlhive  x i y:",x,y);
-  scanf("%f %f ", &x &y);
-  printf("Uneti promenljivu x u dekadnom heksadekadnom oktalnom sistemu:",x);
-  scanf("%d %x %o", &x &x &x);
-  printf("Istampati vrednost x u char:",x);
-  scanf("%c", &x);
-  system("PAUSE");
-  
-    return 0;
-}
-*/
-
-
 //Naci i ispistai na standardnom izlazu zbir.razliku,proizvod i kolicnik dva uneta broja
 
 #include<stdio.h>
