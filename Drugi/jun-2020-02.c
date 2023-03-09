fd7e4e61f93d51a4e12b11a9f867550f7dabb0db
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-09 18:19:44 +0100

    .

--- Drugi/jun-2020-02.c
+++ Drugi/jun-2020-02.c
@@ -1,36 +1,3 @@
-/*Državni univerzitet u Novom Pazaru
-
-www
-
-NOVI PAZAR
-
-OSNOVI PROGRAMIRANJA/PROGRAMIRANJE 1
-
-JUN 2020
-
-1. Sa ulaza se učitavaju prirodni brojevi a I b. Napisati program kojim se 
-ispisuju svi prirodni brojevi iz intervala [a,b] koji su deljivi svim svojim ciframa.
-
-
-2. Napisati program kojim se u niz X umeće broj b neposredno iza minimalnog elementa niza. Unos niza,
-modifikovanje niza I ispis niza odraditi pomoću funkcija. Nije dozvoljeno korišćenje pomoćnog niza
-
-
-
-3. Neka se u datoteci teniseri.txt nalaze podaci o teniserima po sledecem formatu: Sifra igračaž
-(ceo broj), ime (najviše 30 znakova), prezime (najviše 30 znakova) i broj poena na ATP rang listi 
-(ceo broj) U datoteci wimbledon.txt se nalaze podaci o plasmanu tenisera na ovom teniskom turniru.
-U svakom redu datoteke se nalazi šifra tenisera (ceo broj), broj poena koji brani na turniru (ceo broj)
-broj poena koji je osvojio na turniru (ceo broj). Raspored tenisera u datotekama ne mora biti identičan,
-niti svi teniseri iz prve datoteke moraju postojati u drugoj datoteci. Napisati program na programskom jeziku
-C koji pročita sadržaj ulaznih tekst i smešta ih u niz struktura, a zatim u izlaznu datoteku atplista.txt za
-svakog tenisera upiše novi broj poena po formatu kao u prvoj ulaznoj datoteci. Novi broj poena se dobija tako
-što se od starog broja poena na ATP listi oduzme broj poena koje teniser brani, a zatim doda broj poena koje je
-teniser osvojio na turniru
-*/
-
-
-
 // 2. Napisati program kojim se u niz X umeće broj b neposredno iza minimalnog elementa niza. Unos niza,
 // modifikovanje niza I ispis niza odraditi pomoću funkcija. Nije dozvoljeno korišćenje pomoćnog niza
 
