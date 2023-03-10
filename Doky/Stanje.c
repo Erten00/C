bf29e5f27e59f8c9ac42fda0f787b6bd6c384e32
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-10 23:24:35 +0100

    .

--- Doky/Stanje.c
+++ Doky/Stanje.c
@@ -69,7 +69,7 @@ int main() {
     // Izvestaj
     for (int i = 1; i <= BrojPacijenata; i++) {
         int UkupnoZuba = 0, NemaZub = 0, KvarniZub = 0;
-        
+
         for (int j = 0; j < MAX_ZUBA; j++) {
 
             int *Vilica = GornjaVilica;
@@ -82,8 +82,8 @@ int main() {
             }
             if (status == 0) {
                 NemaZub++;
-
-            } else if (status == 2) {
+                }
+ else if (status == 2) {
                 KvarniZub++;
             }
 
