7d37828eba95d606c15804e0d4a6f050df8297d3
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-13 19:50:26 +0100

    .

--- Doky/2.c
+++ Doky/2.c
@@ -11,3 +11,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+// Unos Matrice
+void Unos(int matrix[SIZE][SIZE],int m,int n){
+    int i,j;
+     for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+}
+
+// Ispis Matrice
+void Ispis(int matrix[SIZE][SIZE],int m,int n){
+    int i,j;
+     for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
