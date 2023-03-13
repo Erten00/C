7684e08a41118594933818eee5131f00d1737d94
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-13 19:43:20 +0100

    .

---
+++ Doky/2.c
@@ -0,0 +1,9 @@
+// 2. Na programskom jeziku C napisati strukturni program za prikaz gajbice sa narandžama i praćenje procesa njihovog kvarenja. 
+// Na početku programa korisnik unosi dimenzije gajbice (predstaviti je preko matrice dimenzije MxN) podatke o narandžama u njoj.
+// Svako polje matrice može imati jednu od sledece tri vrednosti: 0-prazno mesto, 1-narandža, 2 – pokvarena narandza. 
+// Smatrati da korisnik nece uneti nevalidne vrednosti. 
+// Unete vrednosti predstavijaju inicijalno stanje gajbice, nakon čega pokvarene narandže počinju "da kvare” svoje susede.
+// Napisati funkciju koja za prosledjenu matricu vraca njeno stanje nakon prvog koraka "kvarenja suseda".
+// U glavnom programu prikazati pocetno stanje gajbice sa narandzama.
+// Pomoću kreirane funkcije simulirati 3 koraka kvarenja narandži i prikazati krajnje stanje.
+// Napomena: Za maksimalan broj poena alocirati memoriju dinamički.
