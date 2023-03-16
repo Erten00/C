686da5ad6600908e79d690d6c49612a22a3fd637
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-16 15:30:44 +0100

    .

--- Prvi/januar2020-nikola.c
+++ Prvi/januar2020-nikola.c
@@ -1,5 +1,5 @@
 /*
-u programskom jeziku c napisati strukturu programa za sumiranje prostih brojeva.
+u programskom jeziku c napisati strukturu programa za sumiranje g.
 najpre uneti maksimalnu sumu a potom unositi cele brojeve . sumirati samo proste brojeve
 dok njihova suma ne postane veća od zadate maksimalne sume.  
 prikazati dobijenu sumu .
@@ -17,12 +17,14 @@ int main(){
     printf("\n");
     while(suma < maksimalnaSuma){
         scanf("%d", &broj);
+        // Odredjivanje prostih brojeva
         for(int i = 2; i < broj; i++){
             if(broj % i == 0){
                 flag = false;
                 break;
             }
         }
+
         if(flag){
             suma = suma + broj;
         }
