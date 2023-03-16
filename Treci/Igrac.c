3d3b7e552fdc18a410a7326c7f2e70974b2bf812
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-16 15:13:53 +0100

    .

--- Treci/Igrac.c
+++ Treci/Igrac.c
@@ -1,13 +1,14 @@
 // Sastaviti program koji iz datoteke igraci.txt čita podatke o igračima 
-//(visina, težina, broj koševa, broj asistencija, broj ukradenih lopti, broj blokada), 
+//(ime, visina, težina, broj koševa, broj asistencija, broj ukradenih lopti, broj blokada), 
 //pronalazi igrača sa najvećim brojem ostvarenih poena na standardnom izlazu ispisuje ime tog igrača i broj ostvarenih poena.
 // Ukupan broj poena računati kao:
 // broj koševa * 1
 // broj asistencija * 0.5
 // broj ukradenih lopti  * 0.3
-//broj blokada * 0.22.
-//Zadatak rešiti upotrebom funkcije za čitanje sadržaja datoteke, 
-//funkcije za ispis podataka i funkcije za računanje ukupnog broja poena.
+// broj blokada * 0.22.
+// Zadatak rešiti upotrebom funkcije za čitanje sadržaja datoteke, 
+// funkcije za ispis podataka i funkcije za računanje ukupnog broja poena.
+
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 100
@@ -24,7 +25,7 @@ typedef struct igrac{
 }IGRAC;
 
 int main(){
-    
+
     IGRAC igraci[MAX];
     int i = 0;
 
