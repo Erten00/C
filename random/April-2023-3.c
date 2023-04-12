a31f389ee43189dbd7422b48649aa29aadd12cf3
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-04-12 17:50:36 +0200

    .

---
+++ random/April-2023-3.c
@@ -0,0 +1,9 @@
+/*
+Napisati program koji vrši obradu nad spiskom odlaznih letova iz nekog grada koji se nalaze u datoteci odlasci.txt. 
+Jedan red datoteke sadrži ime dolaznog grada (jedna reč od maksimalno 20 slova), broj leta (ceo broj) i oznaku aviona (jedna reč od maksimalno 20 slova).
+U datoteci može postojati više letova za jednu destinaciju.
+Datoteka avioni.txt sadrži informacije o avionima.
+Jedan red datoteke sadrži oznaku aviona (jedna reč od maksimalno 20 slova) i broj sedišta u avionu, pri čemu se svaki avion za koji postoji let u datoteci odlasci.txt sigurno pojavljuje u datoteci avioni.txt.
+Program treba da pročita sa standardnog ulaza ime dolaznog grada, da izračuna koliko ukupno postoji sedišta na svim letovima do tog grada i ispiše taj broj na standardnom izlazu. 
+Voditi računa o ispravnoj upotrebi resursa.
+*/
