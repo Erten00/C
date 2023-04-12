6440bb31a21a323a0863da748a24b13c1ddd8899
Erten00 <erten.preljevic.2002@gmail.com>
2023-04-13 01:09:12 +0200

    .

--- random/April-2023-2.c
+++ random/April-2023-2.c
@@ -8,3 +8,68 @@ Učitavanje pojedinačnog niza i proveru da li su dva niza cifara anagrami reali
 Smatrati da su elementi nizova decimalne cifre, kao i da nizovi nemaju više od 100 elemenata. 
 Ukoliko se za dužinu nekog od nizova unese nedozvoljena vrednost, ispisati poruku o greści i prekinuti izvršavanje programa.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_anagram(int *arr1, int *arr2, int len1, int len2) {
+    if (len1 != len2) {
+        return 0;
+    }
+
+    int freq1[10] = {0};
+    int freq2[10] = {0};
+
+    for (int i = 0; i < len1; i++) {
+        freq1[arr1[i]]++;
+        freq2[arr2[i]]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (freq1[i] != freq2[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void load_string(int *arr, int len) {
+    printf("Enter %d digits: ", len);
+    for (int i = 0; i < len; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+int main() {
+    int len1, len2;
+
+    printf("Enter length of first string: ");
+    scanf("%d", &len1);
+    if (len1 < 1 || len1 > 100) {
+        printf("Error: length must be between 1 and 100.\n");
+        return 1;
+    }
+
+    int arr1[len1];
+    load_string(arr1, len1);
+
+    printf("Enter length of second string: ");
+    scanf("%d", &len2);
+    if (len2 < 1 || len2 > 100) {
+        printf("Error: length must be between 1 and 100.\n");
+        return 1;
+    }
+
+    int arr2[len2];
+    load_string(arr2, len2);
+
+    int result = is_anagram(arr1, arr2, len1, len2);
+    if (result) {
+        printf("The two strings are anagrams.\n");
+    } else {
+        printf("The two strings are not anagrams.\n");
+    }
+
+    return 0;
+}
