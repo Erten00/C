a1ca63a80623a04ee0991ea35906cd7146fc26c7
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-04-12 17:42:25 +0200

    .

---
+++ random/April-2023-1.c
@@ -0,0 +1,8 @@
+//Napisati program koji proverava da li su dva niza cifara anagrami.
+//Dva niza cifara su anagrami ako se svaka cifra pojavljuje isti broj puta i u jednom i u drugom nizu.
+//Na primer, nizovi 1 2 3 4 12 3 1 4 jesu anagrami, dok 1 2 3 4 1 2 3 1 1 nisu.
+//Program treba najpre da sa standardnog ulaza učita dužine nizova, a zatim i same elemente nizova,
+//da proveri da li su učitani nizovi anagrami i da na standardnom izlazu ispiše rezultat provere. 
+//Učitavanje pojedinačnog niza i proveru da li su dva niza cifara anagrami realizovati kao zasebne potprograme, koji sa glavnim programom komuniciraju isključivo putem argumenata i povratnih vrednosti. 
+//Smatrati da su elementi nizova decimalne cifre, kao i da nizovi nemaju više od 100 elemenata. 
+//Ukoliko se za dužinu nekog od nizova unese nedozvoljena vrednost, ispisati poruku o greści i prekinuti izvršavanje programa.
