a7a5c4974640ff1a7bbd509afa8b554e4458052a
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-06-07 15:34:36 +0200

    .

--- Treci/Igraci.c
+++ Treci/Igraci.c
@@ -1,40 +1,50 @@
+// Sastaviti program koji iz datoteke igraci txt čita podatke o igračima 
+//(visina, težina, broj koševa, broj asistencija, broj ukradenih lopti, broj blokada), 
+//pronalazi igrača sa najvećim brojem ostvarenih poena na standardnom izlazu ispisuje ime tog igrača i broj ostvarenih poena.
+// Ukupan broj poena računati kao:
+// broj koševa * 1
+// broj asistencija * 0.5
+// broj ukradenih lopti  * 0.3
+//broj blokada * 0.22.
+//Zadatak rešiti upotrebom funkcije za čitanje sadržaja datoteke, 
+//funkcije za ispis podataka i funkcije za računanje ukupnog broja poena.
 #include <stdio.h>
-
-typedef struct igraci {
-  char ip[20];
-  float visina;
-  float tezina;
-  int bk;
-  int ba;
-  int bu;
-  int bb;
-  int poeni;
-}IGRACI;
-
+#include <stdlib.h>
+#define MAX 100
+typedef struct igrac{
+    char ime[20];
+    float visina;
+    float tezina;
+    int brojKoseva;
+    int brojAsistencija;
+    int brojUkradenihLopti;
+    int brojBlokada;
+    float ukupnoPoena;
+}IGRAC;
 int main(){
-  FILe *dat;
-  IGRACI ig[100], najbolji;
-  int i=0, n;
-  dat = fopen("igraci.txt", "r");
-  if(dat == null){
-    printf("greška");
-    exit(1);
-  }
-  while(1){
-    fscanf(dat, "%s %f %f %d %d %d %d", ig[i].ip, ig[i].visina, ig[i].tezina, ig[i].bk, ig[i].ba, ig[i].bu, ig[i].bb);
-    if(feof(dat)){
-      break;
+    FILE * datoteka = fopen("igraci.txt", "r");
+    if(datoteka == NULL){
+        printf("greška");
+        exit(1);
     }
-    ig[i].poeni =  ig[i].bk*1 + ig[i].ba*0.5;
-    i++;
-  }
-  flocse(dat);
-  n=i;
-  najbolji=ig[0];
-  for(i=1; i<n; i++){
-    if(ig[i].poeni > najbolji.poeni);
-    najbolji = igraci[i];
-  }
-  printf("njablji igrač je %s i on je  osvojio %f poena", najbolji.ip,  najbolji.poeni);
-  return 0;
+    IGRAC igraci[MAX];
+    int i = 0;
+    while(!feof(datoteka)){
+        fscanf(datoteka, "%s %f %f %d %d %d %d", igraci[i].ime, &igraci[i].visina, &igraci[i].tezina, &igraci[i].brojKoseva, &igraci[i].brojAsistencija, &igraci[i].brojUkradenihLopti, &igraci[i].brojBlokada);
+        igraci[i].ukupnoPoena = 0;
+        igraci[i].ukupnoPoena += igraci[i].brojKoseva;
+        igraci[i].ukupnoPoena += igraci[i].brojAsistencija * 0.5;
+        igraci[i].ukupnoPoena += igraci[i].brojUkradenihLopti * 0.3;
+        igraci[i].ukupnoPoena += igraci[i].brojBlokada * 0.22;
+        printf("%f \n" ,igraci[i].ukupnoPoena );
+        i++;
+    }
+    IGRAC najbolji = igraci[0];
+    for(int j = 0; j < i; j++){
+        if(igraci[j].ukupnoPoena > najbolji.ukupnoPoena){
+            najbolji = igraci[j];
+        }
+    }
+    printf("najbolji igrac je %s sa %f poena", najbolji.ime, najbolji.ukupnoPoena);
+return 0;
 }
