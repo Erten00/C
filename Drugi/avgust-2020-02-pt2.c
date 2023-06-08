3184e20859f39af35f022df6371968469405b9c6
Erten00 <erten.preljevic.2002@gmail.com>
2023-06-08 15:35:56 +0200

    .

--- Drugi/avgust-2020-02-pt2.c
+++ Drugi/avgust-2020-02-pt2.c
@@ -1,4 +1,4 @@
-// 2. Na programskom jeziku C napisati funkciju rotirajudesno koja vrši rotiranje prosledenog
+// 2. Na programskom jeziku C napisati funkciju rotiraj-u-desno koja vrši rotiranje prosledenog
 // iza dužine N za jedno mesto udesno. U glavnom programu umeti kvadratnu matricu i dimenzija
 // M&M i izvršiti rotiranje njenog oboda za jedno mesto u smeru kazaljke na satu (kao što je prikazano na primeru).
 
@@ -29,20 +29,23 @@ void ispis(int m,int n,int matrica[SIZE][SIZE]){
 void rotiranjeMatrice(int m,int n,int matrica[SIZE][SIZE]){
     int trenutni = 0;
     int prethodni = 0;
-    int red = 0,kolona = 0;
+    int red = 0, kolona = 0;
     prethodni = matrica[red + 1][kolona];
+    
     for(int i = kolona;i < n;i++){
         trenutni = matrica[red][i];
         matrica[red][i] = prethodni;
         prethodni = trenutni;
         }
         red++;
+
         for(int i = red;i < m;i++){
             trenutni = matrica[i][n-1];
             matrica[i][n-1] = prethodni;
             prethodni = trenutni;   
         }
         n--;
+
         if(red < m){
             for(int i = n-1;i >= 0;i--){
                 trenutni = matrica[m-1][i];
@@ -51,6 +54,7 @@ void rotiranjeMatrice(int m,int n,int matrica[SIZE][SIZE]){
             }
         }
         m--;
+
         if(kolona < n){
             for(int i = m-1;i >= 0;i--){
                 trenutni = matrica[i][kolona];
