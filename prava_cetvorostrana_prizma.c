da15c13442aee0759c96a40026dac79d8530ad7b
Erten00 <erten.preljevic.2002@gmail.com>
2023-06-08 15:02:14 +0200

    .

--- prava_cetvorostrana_prizma.c
+++ prava_cetvorostrana_prizma.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
-float povrsina_kvadrata( float a){
-    return sqrt(a, 2);
+float povrsina_kvadrata(float a) {
+    // P = a • a
+    return a * a;
 }
 
-float povrsina_pravougaonika(float a, float b){
-    return  a * b;
+float povrsina_pravougaonika(float a, float b) {
+    // P = a • b
+    return a * b;
 }
 
-float povrsina_prizme(float a, float h){
-    float p, b, m;
-    b = povrsina_kvadrata(a);
+float povrsina_prizme(float a, float h) {
+    // P = 2B + M
+    float b, m;
+    b = 2 * povrsina_kvadrata(a);
     m = 4 * povrsina_pravougaonika(a, h);
-    return b + m; 
+    return b + m;
 }
 
-int main(){
-    float a, h; 
-    printf("Unesite stranicu a i visuinu h: ");
-    scanf("%f %f", &a, &h );
-    printf ("površina prizme je :" povrsina_prizme(a, b));
-    
+int main() {
+    float a, h;
+    printf("Unesite stranicu a i visinu h: ");
+    scanf("%f %f", &a, &h);
+    printf("Površina prizme je: %.2f", povrsina_prizme(a, h));
     return 0;
 }
