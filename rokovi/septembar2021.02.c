915b0293e45bc73abedcc23e37dcb558ce478d8a
Erten00 <erten.preljevic.2002@gmail.com>
2023-06-08 14:16:50 +0200

    .

--- rokovi/septembar2021.02.c
+++ rokovi/septembar2021.02.c
@@ -1,19 +1,11 @@
 // 1. Napisati program koji pomaže zeki da pojede kupus. Na početku programa sa standardnog
-
 // ulaza učitati parametar M koji predstavlja dimenziju polja matrice MxM po kojoj zeka skače.
-
 // Zatim učitati celobrojne vrednosti svih polja matrice. Vrednost 0 predstavlja kupus, 1
-
 // paradajz, 2 krompir, 3 nepoznato. Nakon toga korisnik preko standardnog ulaza unosi
-
 // karakter koji predstavlja pokret zeke (L-levo, D-desno, G-gore, D-dole). Zeka kreče sa
-
 // početne pozicije polja 0,0 i izvršava skokove ukoliko su mogući i jede samo kupus ukoliko
-
 // skoči na polje na kome se nalazi kupus. Na kraju prikazati poruku o količini kupusa koju je
-
 // zeka pojeo i broju skokova koje je izvršio. Prekinuti sa unosom kada korisnik unese karakter
-
 // K. Za unos matrice i unos koraka odraditi posebne funkcije.
 
 
