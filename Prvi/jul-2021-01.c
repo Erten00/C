aff3e8c65fed6280643077dc65d89a696158c91c
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-06-08 19:14:13 +0200

    .

--- Prvi/jul-2021-01.c
+++ Prvi/jul-2021-01.c
@@ -3,47 +3,33 @@
 // na neparnim pozicijama. Primer: Broj 15345 je savršen jer je zbir 1+3+5-5+4
 // dok broj 12345 nije savršen jer 1+3+5/2+4.
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
 
-int obrnutiBroj(int broj){
-    int obrnuti = 0;
-    while(broj != 0){
-        obrnuti = (obrnuti * 10) + (broj % 10);
-        broj /= 10;
+int isPerfect(int number) {
+    int evenSum = 0, oddSum = 0;
+    int digit, isEven = 1;
+
+    while (number > 0) {
+        digit = number % 10;
+        if (isEven) {
+            evenSum += digit;
+        } else {
+            oddSum += digit;
+        }
+        isEven = !isEven;
+        number /= 10;
     }
-    return obrnuti;
+    return (evenSum == oddSum);
 }
 
-void suma(int broj){
-    broj = obrnutiBroj(broj);
-    int sumaNeparnih = 0,sumaParnih = 0,c = 1;
-
-    while(broj != 0){
-        if(c % 2 == 0){
-            sumaParnih += broj % 10;
-        }
-        else{
-            sumaNeparnih += broj % 10;
-        }
-        broj /= 10;
-        c++;
+int main() {
+    int N;
+    printf("Enter a number: ");
+    scanf("%d", &N);
+    if (isPerfect(N)) {
+        printf("The number is perfect.\n");
+    } else {
+        printf("The number is not perfect.\n");
     }
-    printf("Suma parnih:%d\n",sumaParnih);
-    printf("Suma neparnih:%d\n",sumaNeparnih);
-    if(sumaParnih == sumaNeparnih){
-        printf("Broj je savrsen\n");
-    }
-    else{
-        printf("Broj nije savrsen\n");
-    }
-}
-
-int main(){
-    int broj;
-    printf("Unesite broj:");
-    scanf("%d",&broj);
-    suma(broj);
     return 0;
 }
