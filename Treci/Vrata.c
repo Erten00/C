0106499a3b2eaf24d72ae82182dc4b415828597f
Erten00 <erten.preljevic.2002@gmail.com>
2023-06-08 14:16:24 +0200

    .

--- Treci/Vrata.c
+++ Treci/Vrata.c
@@ -1,68 +1,73 @@
-/* formirana je datoteka vrata.txt sa podacima o sastavu ulaznih vrata.  svaki zapis sadrži 
-informacijee: šifra modela vrata, površina vrata napravljenu od plastike, površina vrata napravljena od drveta,
-i površina vrata napravljena od stakla. unesite cene za kvadratni metra plastike, drveta i stakla sa standardnog
-ulaza.
-Pročitati datoteku  i odštampati ukupan broj vrata koja su 100% od drveta.
-Izračunati cenu za svaki model vrata, i upisati u novu datoteku cene.txt  šifra modela vrata, i njegova cena.
-Ispisati na standardnom ulazu cenu najjeftinijih vrata.
+// 3. Formirana je datoteka VRATA DAT sa podacima o sastavu ulaznih vrata. 
+// Svaki zapis sadrži informacije: šifra modela vrata, površinu vrata napravljenu od plastike,
+// površinu vrata napravljenu od drveta površinu vrata napravljenu od stakla. Unesite podatke 
+// o ceni kvadratnog metra plastike, drveta stakla sa standardnog ulaza. Pročitati datoteku i 
+// odštampati ukupan broj vrata koja su 100% od drveta. Izračunati cene za svaki model vrata i u
+// pisati u novu datoteku CENE.DAT -Sifra modela vrata i njegovu cenu. Ispisati na standardnom 
+// izlazu cenu 1 najjeftinijih vrata.
 
-*/
-#include <stdio.h>
-#include <stdlib.h>
+#include <bits/pthreadtypes.h>
+#include <stdarg.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define MAX 100
+#define SIZE 100
 
-typedef struct vrata {
-    int sifra;
-    float p_plastike;
-    float p_drveta;
-    float p_stakla;
+typedef struct vrata{
+    int sifraModelaVrata;
+    int povrsinaPlastike;
+    int povrsinaDrveta;
+    int povrsinaStakla;
     float cena;
-}VRATA;
+
+}VRATA; 
 
 int main(){
-    int od_drveta = 0;
-    int i=0, j=0;
-    float c_plastike;
-    float c_drveta;
-    float c_stakla;
-    printf("Unesite cene za kvadratni metar plastike, drveta i stakla \n");
-    scanf("%f %f %f", &c_plastike, &c_drveta, &c_stakla);
-    FILE *vrata_fajl, *cene_fajl;
-    vrata_fajl = fopen("vrata.txt", "r");
-    if (vrata_fajl == NULL){
-        printf("Greška prilikom otvaranja datoteke vrata.txt");
-        exit(1);
-    }
-    cene_fajl = fopen("cena.txt", "w");
-    if (cene_fajl == NULL){
-        printf("Greška prilikom otvaranja datoteke cena.txt");
+    struct vrata Vrata[SIZE];
+    int cenaPlastike;
+    int cenaDrveta;
+    int cenaStakla;
+    int brojVrata = 0;
+    printf("Unesite cenu po kvadratnom metru plastike:");
+    scanf("%d",&cenaPlastike);
+    printf("Unesite cenu po kvadratnom metru srveta:");
+    scanf("%d",&cenaDrveta);
+    printf("Unesite cenu po kvadratnom metru stakla:");
+    scanf("%d",&cenaStakla);
+    FILE *VRATA,*CENE;
+    VRATA = fopen("Vrata.txt" ,"r");
+    CENE = fopen("Cene.txt" ,"w");
+
+    if(VRATA == NULL){
+        printf("Greska prilikom otvaranja fajla Vrata.txt");
         exit(1);
     }
 
-    VRATA v[MAX];
+    if(CENE == NULL){
+        printf("Greska prilikom otvaranja fajla Cene.txt");
+        exit(1);
+    }
 
-    while(1){
-        if(feof(vrata_fajl)){
-            break;
+
+    while(!feof(VRATA)){
+       
+    for(int i = 0; i < brojVrata;i++){
+        if(fscanf(VRATA, "%d","%d","%d","%d",&Vrata[i].sifraModelaVrata,&Vrata[i].povrsinaPlastike,&Vrata[i].povrsinaDrveta,&Vrata[i].povrsinaStakla)){
+            brojVrata++;
         }
-        else{
-            fscanf(vrata_fajl, "%d %f %f %f", &v[i].sifra, &v[i].p_plastike, &v[i].p_drveta, &v[i].p_stakla);
-            if(v[i].p_drveta>0 && v[i].p_plastike==0 && v[i].p_stakla==0){
-                od_drveta++;
+            if(Vrata[i].povrsinaPlastike > 0 && Vrata[i].povrsinaStakla == 0 && Vrata[i].povrsinaDrveta == 0){
+                printf("Vrata su 100 posto od drveta\n");
             }
-            v[i].cena = v[i].p_plastike * c_plastike + v[i].p_drveta * c_drveta + v[i].p_stakla * c_stakla; 
-            i++;
+            Vrata[i].cena = Vrata[i].povrsinaPlastike * cenaPlastike + Vrata[i].povrsinaDrveta * cenaDrveta + Vrata[i].povrsinaStakla * cenaStakla;
+    }
+
+    }
+    while(!feof(CENE)){
+        for(int i = 0; i < brojVrata; i++){
+            fprintf(CENE, "%d","%f",Vrata[i].sifraModelaVrata,Vrata[i].cena);
         }
     }
-    VRATA najjeftinija =  v[0];
-    for(j=0; j<i; j++){
-        fprintf(cene_fajl, "%d %f\n", v[j].sifra, v[j].cena);
-        if(v[j].cena<najjeftinija.cena){
-            najjeftinija = v[j];
-        }
-    }
-    printf("Cena najjeftinijih vrata je %f", najjeftinija.cena);
-    printf("Ukupno vrata 100 procenata od drveta je %d", od_drveta);    
+
     return 0;
 }
