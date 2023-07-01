b600998bce9f409a9ff6cc5095d8d7d8d880ba37
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-07-01 17:39:23 +0200

    .

---
+++ To Do/Jul-2023-02.c
@@ -0,0 +1,9 @@
+/*
+2. Jedno preduzeće je od banke uzelo kredit u iznosu od K eura sa rokom otplate od n godine I kamatnom stopom od p procenata godišnje. 
+Napisati program koji će Izračunati kolika će biti ukupna kamata.
+Zatim formirati niz koji predstavlja anuitete otplate ako su sve rate jednake(jednake vrednosti uplata glavnice) i ispisati plan otplate kredita.
+Računanje ukupne kamate, formiranje niza anuiteta i ispis plana otplate uraditi
+I=
+K-iznos kredita, p-godišnja kamatna stopa, n- nejednaki anuiteti, I - ukupna kamata (interes), Ik- pripadajući interes k-tom anuitetu.
+Vrednosti K, p, n, se unose sa tastature.
+*/
